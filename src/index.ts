fb1cd0d33a83e3b93b87450c6e9e386bd44cd70b
刘lyxAndy <liuyuxuanandy@outlook.com>
2023-10-22 16:44:53 +0800

    feat: 新目录结构，数据库强类型

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
 import nedb from 'nedb'
 import MessengerServer from './server'
 import { WebSocket } from 'ws'
+import { Group, Message, User } from './types'
 
-const db = new nedb({
+const ChatDB = new nedb<Message>({
     filename: './chat.db',
+    autoload: true
+})
+
+const UserDB = new nedb<User>({
+    filename: './user.db',
+    autoload: true
+})
+
+const GroupDB = new nedb<Group>({
+    filename: './group.db',
+    autoload: true
+})
+
+const LogDB = new nedb({
+    filename: './log.db',
     autoload: true
 })
 
 const server = new MessengerServer()
 
 server.on('__internal_connected',()=>{
-    db.insert({
-        name: 'connection'
+    LogDB.insert({
+        name: 'connection',
+        time: Date.now()
     })
 })
 
-server.on('create_message',(server: WebSocket,message:string)=>{
-    db.insert({
-        name: 'message',
-        data: {
-            message: message
-        }
-    },function(err,doc){
-        server.send(JSON.stringify(
-            {
-                status: 200
-            }
-        ))
-        
-    })
-    
-})
+server.on('create_message',(server: WebSocket,message:string)=>{ })
